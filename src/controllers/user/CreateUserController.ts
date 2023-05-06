bd6ddb531b2e705caf48ac091af35741754f728d
Eduardo Castro <eduardodcastro@gmail.com>
2023-05-06 09:34:03 -0300

    send project github

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -3,6 +3,7 @@ import { CreateUserService } from '../../services/user/CreateUserService'
 
 class CreateUserController {
     async handle(req: Request, res: Response) {
+
         const { name, email, password } = req.body;
 
         // initial service
